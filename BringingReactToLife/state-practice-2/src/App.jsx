6c71a8e5843c876ce65e4428be8db9b31de72da4
johanperjulius1 <johan.per.julius.berglund@gmail.com>
2026-08-26 14:54:07 +0200

    state batch practice

--- BringingReactToLife/state-practice-2/src/App.jsx
+++ BringingReactToLife/state-practice-2/src/App.jsx
@@ -2,15 +2,32 @@ import { useState } from "react"
 
 export default function App() {
 
-  const stateArray = React.useState("initial state value")
-  const state = stateArray[0]
-  const setState = stateArray[1]
+  const [count, setCount] = useState(0)
 
-  // equivalent to:
+  const handleClick = () => {
+    setCount(count => count + 1)
+    setCount(count => count + 1)
+    setCount(count => count + 1)
 
-const [state, setState] = useState("initial state value")
+
+    setCount(count + 1)
+    // setCount(count + 1)
+    // setCount(count + 1)
+  }
+
+//   const stateArray = React.useState("initial state value")
+//   const state = stateArray[0]
+//   const setState = stateArray[1]
+
+//   // equivalent to:
+
+// const [state, setState] = useState("initial state value")
+
 
   return (
-    <></>
+    <>
+    {count}
+     <button onClick={handleClick}>Add 1</button>
+    </>
   )
 }
