2b78825260bb810f7edb98a24a1bb69922d3c5db
johanperjulius1 <johan.per.julius.berglund@gmail.com>
2026-08-27 20:13:22 +0200

    completing clock challenge

--- EscapingReact/ManagingEffects-2/clock/src/App.jsx
+++ EscapingReact/ManagingEffects-2/clock/src/App.jsx
@@ -2,15 +2,17 @@ import { useEffect, useState } from "react";
 
 
 export default function Clock() {
-  const [time, setTime] = useState(Date.now);
+  const [time, setTime] = useState(null);
 
   useEffect( () => {
-    id = setInterval(() => {
-      setTime(time => time + 1)
+   const id = setInterval(() => {
+    setTime(new Date());
     },1000)
 
     return () => clearInterval(id)
   }, [])
+
+  if (time === null) return null;
 
   return (
     <section>
