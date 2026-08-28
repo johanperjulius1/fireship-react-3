97228b075a6b9a86ddcf801a0c261ff3c767b29d
johanperjulius1 <johan.per.julius.berglund@gmail.com>
2026-08-28 10:56:07 +0200

    finishing the country info challenge

---
+++ EscapingReact/ManagingEffects-2/country-info/src/App.jsx
@@ -0,0 +1,86 @@
+import * as React from "react";
+
+export default function CountryInfo() {
+  const [countryCode, setCountryCode] = React.useState("AU");
+  const [data, setData] = React.useState(null);
+  const [isLoading, setIsLoading] = React.useState(true);
+  const [error, setError] = React.useState(null);
+
+  const handleChange = (e) => {
+    setCountryCode(e.target.value)
+  };
+
+
+  React.useEffect(() => {
+    const url = `https://fireship.dev/api/coursesss/react/country-info/${countryCode}`;
+
+    const fetchData = async () => {
+      setIsLoading(true)
+      setError(null)
+
+      try {
+        const response = await fetch(url)
+        if(!response.ok) throw new Error("Fetching failed");
+        const json = await response.json()
+        setData(json)
+      } catch (err) {
+        setError(err)
+      } finally {
+        setIsLoading(false)
+      }
+    }
+    fetchData()
+  }, [countryCode])
+
+  return (
+    <section>
+      <header>
+        <h1>Country Info:</h1>
+
+        <label htmlFor="country">Select a country:</label>
+        <div>
+          <select id="country" value={countryCode} onChange={handleChange}>
+            <option value="AU">Australia</option>
+            <option value="CA">Canada</option>
+            <option value="CN">China</option>
+            <option value="FR">France</option>
+            <option value="DE">Germany</option>
+            <option value="IN">India</option>
+            <option value="JP">Japan</option>
+            <option value="MX">Mexico</option>
+            <option value="GB">United Kingdom</option>
+            <option value="US">United States of America</option>
+          </select>
+          {isLoading && <span>Loading...</span>}
+          {error && <span>{error.message}</span>}
+        </div>
+      </header>
+
+      {data && (
+        <article>
+          <h2>{data.name}</h2>
+          <table>
+            <tbody>
+              <tr>
+                <td>Capital:</td>
+                <td>{data.capital}</td>
+              </tr>
+              <tr>
+                <td>Region:</td>
+                <td>{data.region}</td>
+              </tr>
+              <tr>
+                <td>Population:</td>
+                <td>{data.population}</td>
+              </tr>
+              <tr>
+                <td>Area:</td>
+                <td>{data.area}</td>
+              </tr>
+            </tbody>
+          </table>
+        </article>
+      )}
+    </section>
+  );
+}
