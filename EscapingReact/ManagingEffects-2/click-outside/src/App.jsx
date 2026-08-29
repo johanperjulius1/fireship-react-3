de7618a4b6b68aaf53d469350948289b240e6fdf
johanperjulius1 <johan.per.julius.berglund@gmail.com>
2026-08-29 11:57:29 +0200

    completing click outside challenge

---
+++ EscapingReact/ManagingEffects-2/click-outside/src/App.jsx
@@ -0,0 +1,58 @@
+import "./App.css"
+import * as React from "react";
+
+export default function ClickOutside() {
+  const [isOpen, setIsOpen] = React.useState(false);
+  const modalRef = React.useRef(null);
+
+  const handleOpenModal = () => {
+    setIsOpen(true)
+  };
+
+  const handleCloseModal = () => {
+    setIsOpen(false)
+  };
+
+
+  React.useEffect(() => {
+    if (isOpen) {
+      const handleClick = (e) => {
+        if (modalRef && !modalRef.current.contains(e.target)) {
+          setIsOpen(false);
+        }
+      }
+      addEventListener("pointerdown", handleClick)
+      return () => removeEventListener("pointerdown", handleClick)
+    }
+
+
+  }, [isOpen])
+  // use An effect
+  // effect should depend in isOpen
+  // Effect should set up event listener, and the clean up shall remove it
+
+  // Effect should set up an event handler function
+  // There should be a ref to the modal
+
+
+  return (
+    <>
+      <section>
+        <h1>Click Outside</h1>
+        <button className="link" onClick={handleOpenModal}>
+          Open Modal
+        </button>
+      </section>
+      {isOpen && (
+        <dialog ref={modalRef} >
+          <button onClick={handleCloseModal}>{"X"}</button>
+          <h2>Modal</h2>
+          <p>
+            Click outside the modal to close (or use the button) whatever you
+            prefer.
+          </p>
+        </dialog>
+      )}
+    </>
+  );
+}
