429ab289350d2abaf540ce5cc0c3ff40425d8ef3
johanperjulius1 <johan.per.julius.berglund@gmail.com>
2026-08-29 13:09:58 +0200

    completing auth Context challenge

---
+++ EscapingReact/TeleportationWithContext/auth/src/App.jsx
@@ -0,0 +1,96 @@
+import * as React from "react";
+import Dashboard from "./Dashboard";
+
+const authContext = React.createContext({
+  isAuthenticated: false,
+  login: () => { },
+  logout: () => { }
+});
+
+const AuthProvider = ({ children }) => {
+  const [isAuthenticated, setIsAuthenticated] = React.useState(false);
+
+  const login = () => {
+    setIsAuthenticated(true)
+  };
+
+  const logout = () => {
+    setIsAuthenticated(false)
+  };
+
+  return <authContext.Provider value={{ isAuthenticated, login, logout }}>{children}</authContext.Provider>;
+};
+
+function NavBar() {
+  const { isAuthenticated, logout } = React.useContext(authContext);
+
+  return (
+    <nav>
+      <span role="img" aria-label="Money bags emoji">
+        💰
+      </span>
+      {isAuthenticated && (
+        <button className="link" onClick={logout}>
+          Logout
+        </button>
+      )}
+    </nav>
+  );
+}
+
+function LoginForm() {
+  const { login } = React.useContext(authContext);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // setting up form data with the form target
+    const formData = new FormData(event.target);
+    const username = formData.get("username");
+    const password = formData.get("password");
+    login(username, password);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h2>Log In</h2>
+      <div>
+        <label htmlFor="username">Username:</label>
+        <input
+          required
+          type="text"
+          id="username"
+          name="username"
+          placeholder="Enter your username"
+        />
+      </div>
+      <div>
+        <label htmlFor="password">Password:</label>
+        <input
+          required
+          id="password"
+          type="password"
+          name="password"
+          placeholder="Enter your password"
+        />
+      </div>
+      <button className="primary" type="submit">
+        Login
+      </button>
+    </form>
+  );
+}
+
+function Main() {
+  const { isAuthenticated } = React.useContext(authContext);
+
+  return <main>{isAuthenticated ? <Dashboard /> : <LoginForm />}</main>;
+}
+
+export default function App() {
+  return (
+    <AuthProvider>
+      <NavBar />
+      <Main />
+    </AuthProvider>
+  );
+}
