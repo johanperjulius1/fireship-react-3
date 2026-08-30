9826fb55f3c64827f0599657716a887be614df27
johanperjulius1 <johan.per.julius.berglund@gmail.com>
2026-08-30 14:49:25 +0200

    setting up the task challenge

---
+++ EscapingReact/TeleportationWithContext/tabs/src/App.jsx
@@ -0,0 +1,53 @@
+import * as React from "react";
+
+const tabContext = React.createContext({
+  activeTabValue: null,
+  setActiveTabValue: () => { }
+});
+
+function TabProvider({ children }) {
+  const [activeTabValue, setActiveTabValue] = React.useState()
+
+  return <tabContext.Provider
+    value={{ activeTabValue, setActiveTabValue }}>
+    {children}
+  </tabContext.Provider>;
+}
+
+function TabTrigger({ value }) {
+  const activeTabValue = null;
+  const setActiveTabValue = () => { };
+
+  const handleSetActiveTabValue = () => { };
+
+  return (
+    <button
+      onClick={handleSetActiveTabValue}
+      className={`tab ${activeTabValue === value ? "active" : ""}`}
+    >
+      TODO
+    </button>
+  );
+}
+
+function TabContent() {
+  return null;
+}
+
+export default function App() {
+  return (
+    <section>
+      <h1>Tabs</h1>
+      <TabProvider defaultValue="tab-1">
+        <div className="tabs">
+          <TabTrigger value="tab-1">Tab 1</TabTrigger>
+          <TabTrigger value="tab-2">Tab 2</TabTrigger>
+          <TabTrigger value="tab-3">Tab 3</TabTrigger>
+        </div>
+        <TabContent value="tab-1">Tab Content 1</TabContent>
+        <TabContent value="tab-2">Tab Content 2</TabContent>
+        <TabContent value="tab-3">Tab Content 3</TabContent>
+      </TabProvider>
+    </section>
+  );
+}
