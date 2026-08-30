e4836a9155e8241c47c89e2b6d150c882c5424ad
johanperjulius1 <johan.per.julius.berglund@gmail.com>
2026-08-30 10:31:51 +0200

    completing assignment 5

--- EscapingReact/TeleportationWithContext/js-array-object-practice/src/main.js
+++ EscapingReact/TeleportationWithContext/js-array-object-practice/src/main.js
@@ -7,8 +7,8 @@ const users = {
 // Write a function called getUserName that 
 // takes an id and returns the corresponding name.
 
-function getUserName (id) {
-  if(users[id] === undefined){
+function getUserName(id) {
+  if (users[id] === undefined) {
     return `no user with id ${id} exist`
   }
   return `The name of the user with id ${id} is ${users[id]}`
@@ -34,12 +34,14 @@ const users2 = {
 // getUserInfo("johan", "age");
 // 32
 
+// 3
+
 const getUserInfo = (username, property) => {
-  if(users2[username]===undefined){
+  if (users2[username] === undefined) {
     return `No user by the username ${username} exist`
   }
 
-  if(users2[username][property]===undefined){
+  if (users2[username][property] === undefined) {
     return `No propert like that exist`
   }
   // return `${users2[username][property]}`
@@ -49,4 +51,34 @@ const getUserInfo = (username, property) => {
 // console.log(getUserInfo("anna", "age"))
 console.log(getUserInfo("johan", "id"))
 // console.log(getUserInfo("johan", "age"))
+
+// Exercise 4 — Array of objects
+
+const products = [
+  { id: 1, name: "Laptop", price: 1000 },
+  { id: 2, name: "Phone", price: 500 },
+  { id: 3, name: "Tablet", price: 700 }
+];
+
+function getProduct(name) {
+  console.log(products.find(product => product.name === name))
+}
+
+getProduct("Tablet")
+
+function getProduct2(propertyKey, value) {
+  return console.log(products.find(product => product[propertyKey] === value))
+}
+
+getProduct2("price",  700)
+
+
+// Exercise 5 — Array of objects + function
+
+function getProductName(id){
+  const foundProduct = products.find(product => product.id === id)
+   console.log(foundProduct.name)
+}
+
+getProductName(1)
 
