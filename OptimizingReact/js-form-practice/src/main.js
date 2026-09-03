ca1cfcf6f90c8a71eb530c8e8903e56f10e318f5
johanperjulius1 <johan.per.julius.berglund@gmail.com>
2026-09-03 14:06:20 +0200

    filter practice

--- OptimizingReact/js-form-practice/src/main.js
+++ OptimizingReact/js-form-practice/src/main.js
@@ -58,13 +58,29 @@ function updateCart(state, action) {
 
 console.log(updateCart(state, action))
 
+// Delete / filter practice
+
 const numbers = [1, 2, 3, 4];
 const newNumbers = numbers.filter(number => number !== 3)
 console.log(newNumbers)
 
-function numberFilter (numbersArray, numberToFilter){
+function numberFilter(numbersArray, numberToFilter) {
   return numbersArray.filter(number => number !== numberToFilter)
 }
 
 console.log(numberFilter([3, 4, 6], 4))
+
+const items = [
+  { id: 1, name: "Book" },
+  { id: 2, name: "Pen" },
+  { id: 3, name: "Chair" }
+];
+
+const newItems = items.filter(item => item.id !== 2)
+
+const filterItems = (items, id) => {
+  return items.filter(item => item.id !== id)
+}
+
+console.log(filterItems(items, 1))
 
