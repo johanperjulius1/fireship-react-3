1c36aaaff42af6b8e787f997a434f974cdd0813a
johanperjulius1 <johan.per.julius.berglund@gmail.com>
2026-09-03 11:24:11 +0200

    restarting mini assignments

--- OptimizingReact/js-form-practice/src/main.js
+++ OptimizingReact/js-form-practice/src/main.js
@@ -11,4 +11,58 @@ function increaseByOne() {
 
 console.log(count)
 
-// mini assignments.
+// mini assignments. 1
+
+const fruits = ["apple", "banana"]
+
+const newFruit = "orange"
+const newFruits = [...fruits, newFruit]
+
+// 2
+
+const person = {
+  name: "Johan",
+  age: 32
+}
+
+const newAge = 33
+
+const olderPerson = {
+  ...person,
+  age: newAge
+}
+
+// 3
+
+const items = [
+  {
+    id: 1,
+    name: "book",
+    price: 100
+  }
+]
+
+const newItem = {
+  id: 2,
+  name: "pen",
+  price: 200
+}
+
+const moreItems = [...items, newItem]
+
+// 4
+
+const action = {
+  type: "add",
+  item: {
+    id: 2,
+    name: "Pen",
+    price: 200
+  }
+};
+
+action.item
+
+// 5
+
+const newItems = [...items, action.item ]
