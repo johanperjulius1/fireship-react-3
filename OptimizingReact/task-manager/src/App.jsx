d1bfef3436b9b5ea074da14ac17d110386944996
johanperjulius1 <johan.per.julius.berglund@gmail.com>
2026-09-04 19:27:55 +0200

    setting up the task manager challenge

---
+++ OptimizingReact/task-manager/src/App.jsx
@@ -0,0 +1,56 @@
+import * as React from "react";
+import { createTask } from "./utils";
+import "./App.css"
+
+function reducer(tasks, action) {
+  return tasks;
+}
+
+export default function TaskManager() {
+  const [tasks, dispatch] = React.useReducer(reducer, []);
+
+  const handleUpdateTaskStatus = (id) => {
+    dispatch({ type: "update", id });
+  };
+
+  const handleDeleteTask = (id) => {
+    dispatch({ type: "delete", id });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const formData = new FormData(e.target);
+    dispatch({ type: "add", task: createTask(formData.get("task")) });
+
+    e.target.reset();
+  };
+
+  return (
+    <div>
+      <h1>Task Manager</h1>
+      <form onSubmit={handleSubmit}>
+        <input name="task" placeholder="Task title" />
+        <button className="primary" type="submit">
+          Add Task
+        </button>
+      </form>
+      <ul>
+        {tasks.map((task) => (
+          <li key={task.id}>
+            <div>
+              <button
+                className={`status ${task.status}`}
+                onClick={() => handleUpdateTaskStatus(task.id)}
+              />
+              {task.title}
+            </div>
+            <button className="link" onClick={() => handleDeleteTask(task.id)}>
+              Delete
+            </button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
---
+++ OptimizingReact/task-manager/src/utils.js
@@ -0,0 +1,4 @@
+export function createTask(title) {
+    return { id: Date.now(), title, status: "pending" };
+  }
+  
