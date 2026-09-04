ff5d05af31ebbaf73959a20470a6f9fb8f4290a1
johanperjulius1 <johan.per.julius.berglund@gmail.com>
2026-09-04 18:08:02 +0200

    completing the update case

--- OptimizingReact/js-form-practice/src/main.js
+++ OptimizingReact/js-form-practice/src/main.js
@@ -4,16 +4,13 @@ const action = {
   id: 2
 };
 
-
-console.log(removeItem(items, 2));
-
 // Mini-assignment 6
 
 const state = {
   items: [
-    { id: 1, name: "Book", price: 20 },
-    { id: 2, name: "Pen", price: 10 },
-    { id: 3, name: "Chair", price: 30 }
+    { id: 1, name: "Book", price: 20, quantity: 1 },
+    { id: 2, name: "Pen", price: 10, quantity: 2 },
+    { id: 3, name: "Chair", price: 30, quantity: 1 }
   ],
   total: 60
 };
@@ -38,18 +35,18 @@ const newState = {
 
 // mini assignments for add case
 
-const action = {
+const action2 = {
   type: "add",
-  item: { id: 4, name: "Sofa", price: 30 }
+  item: { id: 4, name: "Sofa", price: 30, quantity: 1 }
 };
 
-const newItems2 = [...state.items, action.item]
-const newTotal = state.total + action.item.price
+const newItems2 = [...state.items, action2.item]
+const newTotal = state.total + action2.item.price
 
 const newState2 = {
   ...state,
-  items: [...state.items, action.item],
-  total: state.total + action.item.price
+  items: [...state.items, action2.item],
+  total: state.total + action2.item.price
 }
 
 function updateCart(state, action) {
@@ -59,13 +56,110 @@ function updateCart(state, action) {
         ...state,
         items: [...state.items, action.item],
         total: state.total + action.item.price
-      }
+      };
 
     case "remove":
       return {
         ...state,
         items: state.items.filter(item => item.id !== action.id),
-        total: state.total - state.items.find(item => item.id === action.id).price
-      }
+        total:
+          state.total -
+          state.items.find(item => item.id === action.id).price
+      };
+
+    case "update": {
+      const updatedItems = state.items.map(item => {
+        if (item.id === action.id) {
+          return {
+            ...item,
+            quantity: action.quantity
+          };
+        }
+
+        return item;
+      });
+
+      const newTotal = updatedItems.reduce(
+        (total, item) => total + item.price * item.quantity,
+        0
+      );
+
+      return {
+        ...state,
+        items: updatedItems,
+        total: newTotal
+      };
+    }
   }
+}
+
+const item = {
+  id: 2,
+  name: "Pen",
+  price: 10,
+  quantity: 2
+};
+
+const updatedItem = {
+  ...item,
+  quantity: 5
+}
+
+// Mini-assignment 2
+// Create a new array where every item has its quantity increased by 1.
+
+const items = [
+  { id: 1, name: "Book", price: 20, quantity: 1 },
+  { id: 2, name: "Pen", price: 10, quantity: 2 },
+  { id: 3, name: "Chair", price: 30, quantity: 1 }
+];
+
+console.log(items.map(item => {
+  return {
+    ...item,
+    quantity: item.quantity + 1
+  }
+}))
+
+const action3 = {
+  type: "update",
+  id: 2,
+  quantity: 5
+};
+
+console.log(items.map(item => {
+  if (item.id === action3.id) {
+    return {
+      ...item,
+      quantity: action3.quantity
+    }
+  }
+  return item;
+}))
+
+// Mini-assignment 4
+// Now let's make the exact same logic, but put it into a variable:
+
+const updatedItems = items.map(item => {
+  if (item.id === action3.id) {
+    return {
+      ...item,
+      quantity: action3.quantity
+    }
+  }
+  return item;
+})
+
+
+// Mini-assignment 5
+
+const newTotal = updatedItems.reduce(
+  (total, item) => total + item.price * item.quantity,
+  0
+);
+
+const newState5 = {
+  ...state,
+  items: updatedItems,
+  total: newTotal
 }
