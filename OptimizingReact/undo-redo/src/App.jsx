5a62ceb52da8d60461538c5c2f53d1f75e4513b4
johanperjulius1 <johan.per.julius.berglund@gmail.com>
2026-09-06 13:21:55 +0200

    setting up increment and decrement actions

--- OptimizingReact/undo-redo/src/App.jsx
+++ OptimizingReact/undo-redo/src/App.jsx
@@ -9,16 +9,33 @@ const initialState = {
 function reducer(state, action) {
   const { past, present, future } = state;
 
-  return state;
+  switch (action.type) {
+    case "increment":
+      return {
+        ...state,
+        present: state.present + 1
+      }
+
+    case "decrement":
+      return {
+        ...state,
+        present: state.present - 1
+      }
+
+  }
 }
 
 export default function CounterWithUndoRedo() {
-  const state = initialState;
+  const [state, dispatch] = React.useReducer(reducer, initialState);
 
-  const handleIncrement = () => {};
-  const handleDecrement = () => {};
-  const handleUndo = () => {};
-  const handleRedo = () => {};
+  const handleIncrement = () => {
+    dispatch({ type: "increment" })
+  };
+  const handleDecrement = () => {
+    dispatch({ type: "decrement" })
+  };
+  const handleUndo = () => { };
+  const handleRedo = () => { };
 
   return (
     <div>
